53900af07da96ec055f3ae6959d24d3ed7030de3
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-08-03 21:30:51 +0200

    fix csproj

--- src/ScenePainter.cs
+++ src/ScenePainter.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Xml;
